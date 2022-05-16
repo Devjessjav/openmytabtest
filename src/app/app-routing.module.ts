b456db6952659ea543e8e0e07ee5eeb77a6957bb
Jesus Ruiz <javier.ruiz2506@gmail.com>
2022-05-16 03:41:51 -0400

    change folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,21 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'walls',
-    loadChildren: () => import('./components/Walls/walls/walls.module').then( m => m.WallsPageModule)
-  },
-  {
-    path: 'room',
-    loadChildren: () => import('./modals/Room/room/room.module').then( m => m.RoomPageModule)
-  },
+  }
 ];
 
 @NgModule({
